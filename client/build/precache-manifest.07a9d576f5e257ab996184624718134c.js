95799a4ea3dd8e2c0ec6b5f43a3518e9fbc26299
Debamitra Mukherjee <dmukherj@progress.com>
2019-08-12 18:32:15 +0530

    added build dir

---
+++ client/build/precache-manifest.07a9d576f5e257ab996184624718134c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8281a79ef4f787f74e1f762f20ae60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff223a5dbf679ebfb01",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2d5c69616cb3879914bb",
+    "url": "/static/js/2.4f5598b4.chunk.js"
+  },
+  {
+    "revision": "eff223a5dbf679ebfb01",
+    "url": "/static/js/main.78ec97d6.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
