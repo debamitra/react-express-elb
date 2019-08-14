40e586abfece81702bd389c6e101d6eb0549833f
Debamitra Mukherjee <dmukherj@progress.com>
2019-08-15 01:18:46 +0530

    Build server and add .ebextensions

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-"use strict";
-
-var _path = _interopRequireDefault(require("path"));
-
-var _express = _interopRequireDefault(require("express"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
 // server.js
 //
-var PORT = process.env.HTTP_PORT || 8081;
-var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
-app.get('/', function (req, res) {
+import path from 'path';
+import express from 'express';
+const PORT = process.env.HTTP_PORT || 4001;
+const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('/', (req, res) => {
   res.send('just gonna send it');
 });
-app.get('/flower', function (req, res) {
+app.get('/flower', (req, res) => {
   res.json({
     name: 'Dandelion',
     colour: 'Blue-ish'
   });
 });
-app.listen(PORT, function () {
-  console.log("Server listening at port ".concat(PORT, "."));
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
 });
